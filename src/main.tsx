e485c31d53e940525f81456aaf3985ccd55a17f8
nutcas3 <maurice.nyanja.e@gmail.com>
2025-10-17 01:17:35 +0300

    feat: initialize MultiversX dApp with browser router and network config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import { initApp } from '@multiversx/sdk-dapp/out/methods/initApp/initApp'
+import type { InitAppType } from '@multiversx/sdk-dapp/out/methods/initApp/initApp.types'
+import { EnvironmentsEnum } from '@multiversx/sdk-dapp/out/types/enums.types'
+import { getNetworkConfig } from './config'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const environment = (import.meta.env.VITE_ENVIRONMENT || 'devnet') as EnvironmentsEnum
+const networkConfig = getNetworkConfig(environment)
+
+const config: InitAppType = {
+  storage: {
+    getStorageCallback: () => localStorage
+  },
+  dAppConfig: {
+    environment,
+    network: networkConfig
+  }
+}
+
+initApp(config).then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  )
+})
