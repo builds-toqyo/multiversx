8ac43d8af4c3cc897a5d1a786fd05cddfc8ba3db
nutcas3 <maurice.nyanja.e@gmail.com>
2025-10-17 01:46:43 +0300

    refactor: move layout and auth logic from App component to Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router'
-import { useGetAccountInfo } from '@multiversx/sdk-dapp/out/react/account/useGetAccountInfo'
 import { routes, routeNames } from '@/routes'
-import Navigation from '@/components/Navigation'
+import { Layout } from '@/components/Layout'
 import Unlock from '@/pages/Unlock'
 import './App.css'
 
 function App() {
-  const { address } = useGetAccountInfo()
-
   return (
-    <div className="min-h-screen bg-gray-50">
-      {address && <Navigation />}
+    <Layout>
       <Routes>
         <Route path={routeNames.unlock} element={<Unlock />} />
         {routes.map((route) => {
           const RouteComponent = route.component
-          
-          if (route.authenticatedRoute && !address) {
-            return (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={<Navigate to={routeNames.unlock} replace />}
-              />
-            )
-          }
 
           return (
             <Route
@@ -36,7 +22,7 @@ function App() {
         })}
         <Route path="*" element={<Navigate to={routeNames.home} replace />} />
       </Routes>
-    </div>
+    </Layout>
   )
 }
 
